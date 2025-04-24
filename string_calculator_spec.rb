a15ddf096dba96bd54eb1fcf57567400454b3736
Pulkit <pulkitengg055@gmail.com>
2025-04-24 10:12:14 +0530

    Update test cases to handle any amount of numbers

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -20,5 +20,19 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1,5")).to eq(6)
       end
     end
+
+    context "with any amount of numbers" do
+      it "returns the sum for three numbers" do
+        expect(StringCalculator.add("1,2,3")).to eq(6)
+      end
+
+      it "returns the sum for five numbers" do
+        expect(StringCalculator.add("10,20,30,40,50")).to eq(150)
+      end
+
+      it "returns the sum for ten numbers" do
+        expect(StringCalculator.add("1,1,1,1,1,1,1,1,1,1")).to eq(10)
+      end
+    end
   end
 end
